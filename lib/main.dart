984b3a75485877fc7f85885fe1bff40e3651687a
victober3 <vicobabolodje@gmail.com>
2021-09-29 01:49:02 -0500

    int

--- lib/main.dart
+++ lib/main.dart
@@ -19,86 +19,74 @@ void main() {
 }
 
 class BMICalculator extends StatelessWidget {
- // const bmi_calculator({Key? key}) : super(key: key);
+  // const bmi_calculator({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(
-
-      ),
-     home: InputPage(),
+      theme: ThemeData.dark(),
+      home: InputPage(),
     );
   }
 }
-class InputPage extends StatelessWidget {
 
+class InputPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text('BMI CALCULATOR'),
+        title: Center(
+          child: Text('BMI CALCULATOR'),
         ),
       ),
       body: Column(
-        children: <Widget> [
-          Expanded(child: Row(
-            children: <Widget> [
-              Expanded(child: Container(
-                  margin: EdgeInsets.all(15.0),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-               ),
-              ),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Reuseable(),
                 ),
+                Expanded(
+                  child: Reuseable(),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
+          Expanded(
+            child: Reuseable(),
           ),
-
-          Expanded(child: Container(
-              margin: EdgeInsets.all(15.0),
-              decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-          ),
-      ),
-
-          Expanded(child: Row(
-            children: <Widget> [
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Reuseable(),
                 ),
-              ),
-              ),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color: Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0),
+                Expanded(
+                  child: Reuseable(),
                 ),
-              ),
-              ),
-            ],
-          ),
+              ],
+            ),
           ),
         ],
-        ),
-        );
+      ),
+    );
   }
 }
 
+class Reuseable extends StatelessWidget {
+  const Reuseable({
+    Key? key,
+  }) : super(key: key);
 
-
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+  }
+}
